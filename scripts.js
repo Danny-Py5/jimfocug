228d2afacb3fa0c653599b497f0bb3ab69c25623
DannyPY <olatundedaniel943@gmail.com>
2025-04-14 06:06:03 -0700

    add email sending to jimfocug using mailjs

--- scripts.js
+++ scripts.js
@@ -31,4 +31,39 @@ document.querySelectorAll(".service__card").forEach((card) => {
     observer.observe(card);
 });
 
-function sendEmail() {}
+const submitPreload = document.querySelector(".submit-preload");
+const modal = document.getElementById("myModal");
+function sendEmail() {
+    submitPreload.style.display = "block";
+    const firstName =
+        document.getElementById("first-name").value || "Anonymous ";
+    const lastName = document.getElementById("last-name").value || "";
+    const param = {
+        name: firstName + lastName,
+        email: document.getElementById("email").value,
+        title: "Message from a client",
+        message: document.getElementById("message").value,
+    };
+
+    emailjs
+        .send("service_2dh578j", "template_hff8tol", param)
+        .then(() => {
+            submitPreload.style.display = "none";
+            modal.showModal();
+        })
+        .catch((error) => {
+            submitPreload.style.display = "none";
+            alert("Email not sent");
+        });
+}
+
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendEmail();
+});
+
+document.getElementById("close-modal").addEventListener("click", () => {
+    document.getElementById("form").reset();
+    document.getElementById("myModal").close();
+});
